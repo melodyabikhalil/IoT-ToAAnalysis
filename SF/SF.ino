50339885487bf4589b5a2a72d87b1b6323b22a04
unknown <soumayard97@hotmail.com>
2019-10-16 17:35:49 +0300

    fixed time in microseconds

--- SF/SF.ino
+++ SF/SF.ino
@@ -1,36 +1,55 @@
-#include <RadioHead.h>
-#include <RHCRC.h>
-#include <RHDatagram.h>
-#include <RHEncryptedDriver.h>
-#include <RHGenericDriver.h>
-#include <RHGenericSPI.h>
-#include <RHHardwareSPI.h>
-#include <RHMesh.h>
-#include <RHNRFSPIDriver.h>
-#include <RHReliableDatagram.h>
-#include <RHRouter.h>
-#include <RHSoftwareSPI.h>
-#include <RHSPIDriver.h>
-#include <RHTcpProtocol.h>
-#include <RH_ASK.h>
-#include <RH_CC110.h>
+// rf95_client.pde
+// Preferably Mega
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messageing client
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example rf95_server
+// Tested with Anarduino MiniWirelessLoRa, Rocket Scream Mini Ultra Pro with the RFM95W
 
 #include <SPI.h>
 #include <RH_RF95.h>
-#include <RH_Serial.h>
-#include <RH_TCP.h>
 
-#include <SPI.h>
-#include <RH_RF95.h>
+
 // Singleton instance of the radio driver
 RH_RF95 rf95;
+//RH_RF95 driver(5, 2); // Rocket Scream Mini Ultra Pro with the RFM95W
+
+// Need this on Arduino Zero with SerialUSB port (eg RocketScream Mini Ultra Pro)
+//#define Serial SerialUSB
+
+//The parameter are pre-set for 868Mhz used. If user want to use lower frenqucy 433Mhz.Better to set 
+//rf95.setSignalBandwidth(31250);
+//rf95.setCodingRate4(8);
 float frequency = 868.1;
-void setup() {
-  // put your setup code here, to run once:
+
+void setup()
+{
+  // Rocket Scream Mini Ultra Pro with the RFM95W only:
+  // Ensure serial flash is not interfering with radio communication on SPI bus
+  //  pinMode(4, OUTPUT);
+  //  digitalWrite(4, HIGH);
+
   Serial.begin(115200);
-  while (!Serial) ;
-   rf95.setFrequency(frequency);
-    // Setup Power,dBm
+  while (!Serial) ; // Wait for serial port to be available
+  //Serial.println("initialization");
+  if (!rf95.init())
+    //Serial.println("init failed");
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // you can set transmitter powers from 5 to 23 dBm:
+  //  driver.setTxPower(23, false);
+  // If you are using Modtronix inAir4 or inAir9,or any other module which uses the
+  // transmitter RFO pins and not the PA_BOOST pins
+  // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true.
+  // Failure to do that will result in extremely low transmit powers.
+  //  driver.setTxPower(14, true);
+
+  rf95.setFrequency(frequency);
+  // Setup Power,dBm
   rf95.setTxPower(13);
 
   // Setup Spreading Factor (6 ~ 12)
@@ -43,12 +62,29 @@ void setup() {
   // Setup Coding Rate:5(4/5),6(4/6),7(4/7),8(4/8) 
   rf95.setCodingRate4(5);
   
-
+  /*
+  //Different Combination for distance and speed examples: 
+  Example 1: Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on. Default medium range
+    rf95.setSignalBandwidth(125000);
+    rf95.setCodingRate4(5);
+    rf95.setSpreadingFactor(7);
+  Example 2: Bw = 500 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on. Fast+short range
+    rf95.setSignalBandwidth(500000);
+    rf95.setCodingRate4(5);
+    rf95.setSpreadingFactor(7);
+  Example 3: Bw = 31.25 kHz, Cr = 4/8, Sf = 512chips/symbol, CRC on. Slow+long range
+    rf95.setSignalBandwidth(31250);
+    rf95.setCodingRate4(8);
+    rf95.setSpreadingFactor(9);
+  Example 4: Bw = 125 kHz, Cr = 4/8, Sf = 4096chips/symbol, CRC on. Slow+long range
+    rf95.setSignalBandwidth(125000);
+    rf95.setCodingRate4(8);
+    rf95.setSpreadingFactor(12); 
+  */
 }
-
 unsigned long begin_time;
 unsigned long end_time;
-int SF;
+int long SF;
 
 void loop()
 {
@@ -69,7 +105,7 @@ void loop()
  
   unsigned long ToA = end_time-begin_time;
 
-  Serial.print("soumaya ");
+  
   Serial.print(SF);
   Serial.print(" ");
   Serial.print(ToA);
@@ -119,5 +155,9 @@ void loop()
   
 
   
-  delay(10000);
+  
+  
+
+  
+ 
 }
--- save_data.py
+++ save_data.py
@@ -8,12 +8,16 @@ import time
 
 def save_data(path,iterations):
     ser = serial.Serial("COM6",115200)
+    print('1')
     time.sleep(2) #wait for arduino
+    print('finish waiting')
     file = open(path,"w")
     i = 0
     while (i<iterations):
+        print('before readline')
         line = ser.readline()
-        line = line.decode("utf-8") 
+        line = line.decode("utf-8")
+        print(line)
         file.write(line)
         i = i + 1
     ser.close()
